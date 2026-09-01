2116b71a225860f65f490a56c019180efc2a1958
pad <yoann.padioleau@gmail.com>
2026-09-01 17:36:30 +0200

    CapStdlib.ml: merge with the one in xix

--- src/caps/CapStdlib.mli
+++ src/caps/CapStdlib.mli
@@ -1,3 +1,7 @@
 (* Capability-aware wrappers of the dangerous functions in Stdlib.ml *)
 
+(* deprecated, use Exit.exit now *)
 val exit : < Cap.exit; .. > -> int -> 'a
+
+(* deprecated, use FS.with_open_in now *)
+val open_in : < Cap.open_in; ..> -> string -> in_channel
