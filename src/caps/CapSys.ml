cec6e595d0e5fbdc8d380fe85dabedae7f5e1a05
pad <yoann.padioleau@gmail.com>
2026-09-01 17:35:34 +0200

    CapSys.ml: merge with the one in xix

--- src/caps/CapSys.ml
+++ src/caps/CapSys.ml
@@ -1,6 +1,28 @@
-let argv _caps = Sys.argv
-let set_signal _caps = Sys.set_signal
-let chdir _caps = Sys.chdir
-let command _caps = Sys.command
-let getenv _caps = Sys.getenv
-let getenv_opt _caps = Sys.getenv_opt
+
+let chdir (caps : < Cap.chdir; ..>) = 
+  let _ = caps#chdir in
+  (* nosemgrep: use-caps *)
+  Sys.chdir
+
+let argv (caps : < Cap.argv; .. >) = 
+  let _ = caps#argv in
+  (* nosemgrep: do-not-use-argv *)
+  Sys.argv
+
+let getenv (caps : < Cap.env; ..>) = 
+  let _ = caps#env in
+  (* nosemgrep: use-caps *)
+  Sys.getenv
+(* TODO: add to ocaml-light
+let getenv_opt (caps : < Cap.env; ..>) = 
+  let _ = caps#env in
+  (* nosemgrep: use-caps *)
+  Sys.getenv_opt
+*)
+
+let command (caps : < Cap.fork; Cap.exec; Cap.wait; .. >) cmd = 
+  let _ = caps#fork in
+  let _ = caps#exec cmd in
+  let _ = caps#wait in
+  (* nosemgrep: use-caps *)
+  Sys.command cmd
